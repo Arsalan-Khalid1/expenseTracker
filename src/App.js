1e08b02a01a2f9f09294eeea4c992685b10327ac
Arsalan Khalid <arslankhalid889@gmail.com>
2021-08-03 23:03:07 -0700

    header and balance component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import "./App.css";
+import Balance from "./components/Balance";
+import Header from "./components/Header";
 
 function App() {
-  return <div>Let's start</div>;
+  return (
+    <div>
+      <Header />
+      <div className="container">
+        <Balance />
+      </div>
+    </div>
+  );
 }
 
 export default App;
